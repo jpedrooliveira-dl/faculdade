e5568aa130c6d1d17b4914968220d1141beeccf5
Joao Pedro O. <tuquinhatuca15@gmail.com>
2024-09-08 10:00:34 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+function pesquisar () {
+    console.log();
+
+    let selecao = document.getElementById("resultados-pesquisa-sistema");
+
+    let resultadoAqui = document.getElementById("resultado_aqui").value
+
+    //Se resultadoAqui  for uma sting sem nada
+    if (resultadoAqui == "") {
+
+        selecao.innerHTML = "<p>Nada foi encontrado :(</p>";
+        const paragrafo = document.querySelector('p');
+paragrafo.style.color = '#b7c2d7';
+        return
+    }
+
+    resultadoAqui = resultadoAqui.toLowerCase()
+
+    let resultadoaqui = " "
+    let nome = "";
+    let descricao = "";
+    let tags = "";
+    
+    for (let dado of dados) {
+        titulo = dado.nome.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        //se titulo includes resultadoAqui
+        if (nome.includes(resultadoAqui) || descricao.includes(resultadoAqui) || tags.includes(resultadoAqui)){
+
+            // cria um novo elemento
+            resultadoaqui += `
+    
+            <h2 class="h2sistema"> ${dado.nome}</h2>
+            
+                       <div class="item-resultado">
+                      <p class="paragrafosistema"> ${dado.descricao}</p>
+                      <a class="asistema" href= ${dado.link} target="_blank">Saiba mais</a>
+                       </div>
+            
+            `
+        }  
+        
+    }
+
+    if (!resultadoaqui) {
+        resultadoaqui = "<p>Nada encontrado!</p>"
+        return
+    }
+    
+    selecao.innerHTML = resultadoaqui
+
+}
+
+
---
+++ dados.js
@@ -0,0 +1,52 @@
+let dados = [
+    {
+        nome : "Sistema de Informação",
+        descricao : "A Faculdade de Sistemas de Informação é a porta de entrada para um universo de possibilidades na era digital. Ao longo do curso, os estudantes adquirem conhecimentos sólidos em programação, bancos de dados, redes de computadores e gestão de projetos, desenvolvendo habilidades essenciais para criar e gerenciar soluções tecnológicas inovadoras.",
+        link : "https://querobolsa.com.br/cursos-e-faculdades/sistemas-de-informacao",
+        tags : ""
+    },
+    {
+        nome: "Ciência da Computação",
+        descricao: "A Ciência da Computação é a base para a inovação tecnológica. O curso explora os fundamentos teóricos da computação, desde algoritmos e estruturas de dados até inteligência artificial e teoria da computação. Os graduados em Ciência da Computação são capazes de desenvolver soluções computacionais complexas, conduzir pesquisas e impulsionar a evolução da tecnologia.",
+        link: "https://querobolsa.com.br/cursos-e-faculdades/ciencia-da-computacao"
+      },
+      {
+        nome: "Engenharia de Software",
+        descricao: "A Engenharia de Software transforma ideias em software de alta qualidade. O curso ensina os princípios e práticas para projetar, desenvolver, testar e implantar sistemas de software de forma eficiente e confiável. Engenheiros de software são profissionais indispensáveis para o desenvolvimento de aplicações, sistemas operacionais e outras soluções de software que moldam o mundo digital.",
+        link: "https://querobolsa.com.br/cursos-e-faculdades/engenharia-de-software"
+      },
+]
+
+
+let filmeti = [
+        {
+          nome: "O Social",
+          sinopse: "A história de Mark Zuckerberg, um jovem estudante de Harvard que funda o Facebook, revolucionando a maneira como as pessoas se conectam.",
+          lancamento: 2010,
+          classificacaoIndicativa: "12 anos"
+        },
+        {
+          nome: "O Jogo da Imitação",
+          sinopse: "A história real de Alan Turing, o matemático britânico que liderou um grupo de especialistas em Bletchley Park na tentativa de decifrar a máquina Enigma, utilizada pelos nazistas durante a Segunda Guerra Mundial.",
+          lancamento: 2014,
+          classificacaoIndicativa: "12 anos"
+        },
+        {
+          nome: "Piratas da Sillicon Valley",
+          sinopse: "O filme narra a história da rivalidade entre Steve Jobs e Bill Gates, os fundadores da Apple e da Microsoft, respectivamente, e como suas empresas moldaram a indústria da tecnologia.",
+          lancamento: 1999,
+          classificacaoIndicativa: "12 anos"
+        },
+        {
+          nome: "Ex Machina",
+          sinopse: "Um programador ganha a oportunidade de passar uma semana com um androide de inteligência artificial, desenvolvido por seu chefe, e se vê envolvido em uma complexa relação com a máquina.",
+          lancamento: 2015,
+          classificacaoIndicativa: "14 anos"
+        },
+        {
+          nome: "Her",
+          sinopse: "Em um futuro próximo, um homem solitário se apaixona por um sistema operacional com inteligência artificial, desenvolvido para atender às suas necessidades emocionais.",
+          lancamento: 2013,
+          classificacaoIndicativa: "12 anos"
+        }
+      ];
