4a3363a151c1c95c0855996f54bcc50b75502e4d
Joao Pedro O. <tuquinhatuca15@gmail.com>
2024-10-03 18:09:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
-function pesquisar () {
-    console.log();
+    function pesquisar () {
+        console.log();
 
-    let selecao = document.getElementById("resultados-pesquisa-sistema");
+        let selecao = document.getElementById("resultados-pesquisa-sistema");
 
-    let resultadoAqui = document.getElementById("resultado_aqui").value
+        let resultadoAqui = document.getElementById("resultado_aqui").value
 
-    //Se resultadoAqui  for uma sting sem nada
-    if (resultadoAqui == "") {
+        //Se resultadoAqui  for uma sting sem nada
+        if (resultadoAqui == "") {
 
-        selecao.innerHTML = "<p>Nada foi encontrado :(</p>";
-        const paragrafo = document.querySelector('p');
-paragrafo.style.color = '#b7c2d7';
-        return
-    }
+            selecao.innerHTML = "<p>Nada foi encontrado  :(</p>";
+            const paragrafo = document.querySelector('p');
+    paragrafo.style.color = '#b7c2d7';  
+            return
+        } 
 
-    resultadoAqui = resultadoAqui.toLowerCase()
+        resultadoAqui = resultadoAqui.toLowerCase()
 
-    let resultadoaqui = " "
-    let nome = "";
-    let descricao = "";
-    let tags = "";
+        let resultadoaqui = " "
+        let nome = "";
+        let descricao = "";
+        let tags = "";
 
-    // let encontrouResultado = false;
-    
-    for (let dado of dados) {
-        titulo = dado.nome.toLowerCase()
-        descricao = dado.descricao.toLowerCase()
-        tags = dado.tags.toLowerCase()
-        //se titulo includes resultadoAqui
-        if (nome.includes(resultadoAqui) || descricao.includes(resultadoAqui) || tags.includes(resultadoAqui)){
-
-            // cria um novo elemento
-            resultadoaqui += `
-    
-            <h2 class="h2sistema"> ${dado.nome}</h2>
-            
-                       <div class="item-resultado">
-                      <p class="paragrafosistema"> ${dado.descricao}</p>
-                      <a class="asistema" href= ${dado.link} target="_blank">Saiba mais</a>
-                       </div>
-            
-            `
-        }  
+        // let encontrouResultado = false;
         
+        for (let dado of dados) {
+            titulo = dado.nome.toLowerCase()
+            descricao = dado.descricao.toLowerCase()
+            tags = dado.tags.toLowerCase()
+            //se titulo includes resultadoAqui
+            if (nome.includes(resultadoAqui) || descricao.includes(resultadoAqui) || tags.includes(resultadoAqui)){
+
+                // cria um novo elemento
+                resultadoaqui += `
+        
+                <h2 class="h2sistema"> ${dado.nome}</h2>
+                
+                        <div class="item-resultado">
+                        <p class="paragrafosistema"> ${dado.descricao}</p>
+                        <a class="asistema" href= ${dado.link} target="_blank">Saiba mais</a>
+                        </div>
+                
+                `
+            }  
+            
+        }
+
+        
+
+        // if (!encontrouResultado) {
+        //     resultadoaqui = `<p style="color: #b7c2d7;">Nada encontrado!</p>`;
+        // }
+        
+        
+        selecao.innerHTML = resultadoaqui
+
     }
-
-    // if (!encontrouResultado) {
-    //     resultadoaqui = `<p style="color: #b7c2d7;">Nada encontrado!</p>`;
-    // }
-    
-    
-    selecao.innerHTML = resultadoaqui
-
-}
-
-
